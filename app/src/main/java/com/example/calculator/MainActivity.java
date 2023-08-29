2b40a3e93bc1b418de8a749f529fb8467a965424
Jay1570 <jaygheewala157@gmail.com>
2023-08-29 09:33:43 +0530

    Bug Fixes

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.calculator;
 
-import android.icu.text.DecimalFormat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -10,8 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    TextView result,solution;
-    int i=0;
+    char []s=new char[50];
+    double []e=new double[50];
+    public TextView result,solution;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,57 +49,141 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         MaterialButton b=(MaterialButton) v;
         String btext=b.getText().toString();
         String cal=solution.getText().toString();
-        switch(btext){
-            case "C":
-                if(cal.length()>0) {
-                    cal = cal.substring(0, cal.length() - 1);
-                }
-                solution.setText(cal);
-                break;
-            case "AC":
-                solution.setText("");
-                result.setText("0");
-                break;
-            case "=":
-                solution.setText(result.getText());
-                break;
-            default:
-                cal+=btext;
-                solution.setText(cal);
+        if(btext.equals("AC")){
+            solution.setText("");
+            result.setText("0");
+            return;
         }
-        result.setText(getresult(cal));
+        if(btext.equals("=")){
+            solution.setText(result.getText());
+            return;
+        }
+        if(btext.equals("C")){
+            if(cal.length()>0)
+                cal=cal.substring(0,cal.length()-1);
+        }
+        else{
+            cal=cal+btext;
+        }
+        String result1=getresult(cal);
+        if(!result1.equals("Err"))
+            result.setText(result1);
+        solution.setText(cal);
     }
-
-
     String getresult(String d){
-        double result = 0;
-        try {
-            String[] operands=d.split("\\+|-|\\*|/");
-            String[] operators=d.split("\\d*\\.?\\d+");
-            Double num1=Double.parseDouble(operands[0]);
-            Double num2=Double.parseDouble(operands[1]);
-            switch (operators[1]) {
-                case "+":
-                    result = num1 + num2;
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    break;
-                case "/":
-                    result = num1 / num2;
-                    break;
+        Evaluate e1 = new Evaluate();
+        double op1, op2, r = 0;
+        StringBuilder temp = new StringBuilder();
+        Stack st=new Stack();
+        char []in= d.toCharArray();
+        char []post=new char[in.length*2];
+        int j=0;
+        for(char c:in){
+            switch (c) {
+                case '(' -> st.push(c);
+                case '+', '-', '*', '/' -> {
+                    while (st.top>-1&&st.priority(s[st.top]) >= st.priority(c)) {
+                        post[j++] = ' ';
+                        post[j++] = st.pop();
+                    }
+                    post[j++] = ' ';
+                    st.push(c);
+                }
+                case ')' -> {
+                    while (s[st.top] != '(') {
+                        post[j++] = ' ';
+                        post[j++] = st.pop();
+                    }
+                    post[j++] = ' ';
+                    st.pop();
+                }
+                default -> post[j++] = c;
             }
-            if(result%1==0){
-                DecimalFormat f = new DecimalFormat("0");
-                return f.format(result);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-
-        return String.valueOf(result);
+        while(st.top>-1) {
+            post[j++] = ' ';
+            post[j++] = st.pop();
+        }
+        try {
+            for (char c : post) {
+                switch (c) {
+                    case '*' -> {
+                        op2 = e1.pop();
+                        op1 = e1.pop();
+                        r = op1 * op2;
+                        e1.push(r);
+                    }
+                    case '/' -> {
+                        op2 = e1.pop();
+                        op1 = e1.pop();
+                        r = op1 / op2;
+                        e1.push(r);
+                    }
+                    case '+' -> {
+                        op2 = e1.pop();
+                        op1 = e1.pop();
+                        r = op1 + op2;
+                        e1.push(r);
+                    }
+                    case '-' -> {
+                        op2 = e1.pop();
+                        op1 = e1.pop();
+                        r = op1 - op2;
+                        e1.push(r);
+                    }
+                    case ' ' -> {
+                        if (!temp.toString().equals("")) {
+                            e1.push(Double.parseDouble(temp.toString()));
+                        }
+                        temp = new StringBuilder();
+                    }
+                    default -> temp.append(c);
+                }
+            }
+            return String.valueOf(r);
+        }
+        catch (Exception e){
+            return "Err";
+        }
+    }
+    public class Stack {
+        public  int top=-1;
+        void push(char d) {
+            top=top+1;
+            s[top] = d;
+        }
+        char pop() {
+            char d = s[top];
+            top = top - 1;
+            return d;
+        }
+        int priority(char d) {
+            switch (d) {
+                case '*', '/' -> {
+                    return 2;
+                }
+                case '+', '-' -> {
+                    return 1;
+                }
+                default -> {
+                    return 0;
+                }
+            }
+        }
+    }
+    public class Evaluate {
+        public int top=-1;
+        void push(double d) {
+            top=top+1;
+            e[top] = d;
+        }
+        double pop() {
+            if(top>-1) {
+                double d = e[top];
+                top=top-1;
+                return d;
+            }
+            return 0;
+        }
     }
 }
